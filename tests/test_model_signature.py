5a72f36c9c78eae6ce5dad42293c2b4aae9c3128
Vaibhav Pal <vaibhavkumar8979581578@gmail.com>
2026-01-03 20:24:48 +0530

    change in many files to setup the singnature file test

--- tests/test_model_signature.py
+++ tests/test_model_signature.py
@@ -1,13 +1,17 @@
-# tests/test_model_registry.py
-
 import mlflow
-import numpy as np
 import pytest
+import numpy as np
 
 MLFLOW_TRACKING_URI = "http://ec2-13-62-47-8.eu-north-1.compute.amazonaws.com:5000"
 MODEL_NAME = "yt_chrome_plugin_model"
 STAGE = "Staging"
 
+@pytest.fixture(scope="session")
+def model_info():
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    model_uri = f"models:/{MODEL_NAME}/{STAGE}"
+    info = mlflow.models.get_model_info(model_uri)
+    return info
 
 @pytest.fixture(scope="session")
 def model():
@@ -15,26 +19,22 @@ def model():
     model_uri = f"models:/{MODEL_NAME}/{STAGE}"
     return mlflow.pyfunc.load_model(model_uri)
 
-
 def test_model_loads(model):
     assert model is not None
 
+def test_model_has_signature(model_info):
+    signature = model_info.signature
+    assert signature is not None, "Model signature missing"
 
-def test_model_has_signature(model):
-    signature = model.metadata.signature
-    assert signature is not None, " Model signature missing"
+def test_model_has_input_example(model_info):
+    input_example = model_info.input_example
+    assert input_example is not None, "Input example missing"
 
-
-def test_model_has_input_example(model):
-    input_example = model.metadata.load_input_example()
-    assert input_example is not None, " Input example missing"
-
-
-def test_model_prediction(model):
-    input_example = model.metadata.load_input_example()
+def test_model_prediction(model, model_info):
+    # Load input example
+    input_example = model_info.input_example
     X = np.array(input_example)
 
     preds = model.predict(X)
-
     assert preds is not None
     assert len(preds) == X.shape[0]
